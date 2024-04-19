09611388d800bea7e7e8588347d8fba778b9b5ae
imane <imanezenasnii@gmail.com>
2024-04-19 20:58:04 +0100

    Add states method

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,11 +4,22 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 from flask import jsonify
+
+
+@app_views.route("/states/<state_id>", strict_slashes=False, methods=["GET"])
 @app_views.route("/states", strict_slashes=False, methods=["GET"])
-def get_states():
-    """ Returns states """
-    result = []
-    data = storage.all(State)
-    for obj in data.values():
-        result.append((obj).to_dict())
-    return jsonify(result)
+def states(state_id=None):
+    """return a JSON: list of all State objects or one State,
+    Or not found if id not exsit"""
+    if state_id is None:
+        result = []
+        states = storage.all(State).values()
+        for state in states:
+            result.append(state.to_dict())
+        return jsonify(result)
+    else:
+        result = []
+        key = f"State.{state_id}"
+        states = storage.all(State)[key]
+        return jsonify(states.to_dict())
+
